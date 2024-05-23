b3586516f01aa4a7b3ebaf84059f0c967bca62e0
jadedrb <jadecdrb@gmail.com>
2024-05-23 16:36:23 -0400

    added cors middleware

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,16 @@
 import express from 'express'
-
+import cors from 'cors'
 import 'dotenv/config'
 
 import mongoConfig from './config.js'
-
 import todoRoutes from './routes/todoRoutes.js'
 
 const app = express()
 
 const port = 8080
 
+app.use(cors())
 app.use(express.json())
-
 app.use('/api/todos', todoRoutes)
 
 app.get('/', (req, res) => {
